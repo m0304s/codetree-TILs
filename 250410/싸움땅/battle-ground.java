c4ae074e3ea8dc20e44009df413109fb013497c3
MinSeok_Choi <120546936+m0304s@users.noreply.github.com>
2025-04-10 11:12:50 +0900

    Title: 싸움땅, Time: 99ms, Memory: 9MB, Status: Runtime Error - Codetree

--- 250410/싸움땅/battle-ground.java
+++ 250410/싸움땅/battle-ground.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class 싸움땅 {
+public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	
